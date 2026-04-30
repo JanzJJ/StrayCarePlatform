4be3edb7bbc19234bc1a5b6fd6b4352d4dbcad93
Nethma Jatunarachchi <147967223+JanzJJ@users.noreply.github.com>
2026-04-30 22:37:51 +0100

    Backend server update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,10 @@ import App from "./App.jsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render( 
   <React.StrictMode>
     {/* Replace with your actual Google Client ID */}
-    <GoogleOAuthProvider clientId="559077721257-tmmo8kig7jcjijmtdn2jr6rdp59job32.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="784469257846-88j86vscn24q3mlqa29hiibs33bd9h5u.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>,
